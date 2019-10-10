40159fac03cf9ab47684ce574406246506d52e33
Jack Moseley <jackwmoseley@gmail.com>
2019-10-10 10:10:34 +0100

    Added a space in a comment

--- problem-12.js
+++ problem-12.js
@@ -19,6 +19,7 @@
  * We can see that 28 is the first triangle number to have over five divisors.
 
  * What is the value of the first triangle number to have over five hundred divisors?
+ *
  */
 
  function findNextTriangleNumber(index) {
